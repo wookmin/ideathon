99cb5f602c75bf3368e0f6b863964e5efe79c976
wookmin <alsdnr030226@naver.com>
2026-05-17 23:33:22 +0900

    ddddd

--- lib/config/env.dart
+++ lib/config/env.dart
@@ -1,3 +1,5 @@
+import 'package:flutter/foundation.dart';
+
 import 'env_local.dart' as local;
 
 class Env {
@@ -17,8 +19,14 @@ class Env {
     defaultValue: local.mapKey,
   );
 
-  static const backendBaseUrl = String.fromEnvironment(
-    'BACKEND_BASE_URL',
-    defaultValue: 'http://127.0.0.1:4000',
-  );
+  static String get backendBaseUrl {
+    const configured = String.fromEnvironment(
+      'BACKEND_BASE_URL',
+      defaultValue: '',
+    );
+    if (configured.isNotEmpty) {
+      return configured;
+    }
+    return kIsWeb ? '' : 'http://127.0.0.1:4000';
+  }
 }
