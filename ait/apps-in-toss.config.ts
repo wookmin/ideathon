92611b2944ad6df190eae35ca1e15a37518ae729
wookmin <alsdnr030226@naver.com>
2026-09-10 15:04:14 +0900

    chore: update appName from meomchit to mumchit

--- ait/apps-in-toss.config.ts
+++ ait/apps-in-toss.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from '@apps-in-toss/web-framework/config';
 
 export default defineConfig({
   // 앱인토스 콘솔의 appName과 반드시 같게 바꿔주세요.
-  appName: 'meomchit',
+  appName: 'mumchit',
   brand: {
     primaryColor: '#0F6DF3',
   },
